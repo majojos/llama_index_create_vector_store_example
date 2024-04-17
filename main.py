436b05524d88d6484831f961dd6ea86731d259a4
jordan <jordan@jordan.jordan>
2024-04-17 20:32:16 +0200

    add code to create vector store

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+import os
+import faiss
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, Settings
+from llama_index.vector_stores.faiss import FaissVectorStore
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-documents = SimpleDirectoryReader()
+
+def main():
+    os.environ['HF_HOME'] = '/mnt/big_part/projects/12_llamaindex_from_documents_example/transformers'
+
+    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
+    faiss_index = faiss.IndexFlatL2(384)
+    documents = SimpleDirectoryReader("/mnt/big_part/projects/12_llamaindex_from_documents_example/data").load_data(
+        show_progress=True)
+
+    vector_store = FaissVectorStore(faiss_index=faiss_index)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, show_progress=True)
+
+    index.storage_context.persist("/mnt/big_part/projects/12_llamaindex_from_documents_example/storage")
+
+
+if __name__ == '__main__':
+    main()
